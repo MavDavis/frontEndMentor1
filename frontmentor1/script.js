bdcecc290ad7e6cfcde41c2418d315e541125e41
MavDavis <90147670+MavDavis@users.noreply.github.com>
2022-02-24 15:11:08 +0100

    Add files via upload

---
+++ frontmentor1/script.js
@@ -0,0 +1,15 @@
+ alert("hello world");
+let n = Math.random();
+n = n * 100;
+ let m = Math.round(n) ;
+
+
+     if ( m > 50){
+     console.log("your love is super strong and rated "+ m +" %"); }
+        else {
+            console.log( "you don't have a future in this relationship. your love life is " + m );
+       
+     }
+  
+ 
+
